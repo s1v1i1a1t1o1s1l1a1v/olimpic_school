26839e5bb4b5b1bba40185db13d919dc0ad6b0c4
zaiatsFlagbit <oleg.zaiats@flagbit.de>
2023-03-25 12:44:55 +0200

    abstraction and interfaces

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php 
+
+
+interface sportActivity {
+   
+	private function compete(); 
+
+	private function condition();
+
+}
+
+
+
+class  abstract Sport {
+
+   $rules; 
+ 
+   $uniform;
+
+   $coach;
+
+   $inventory;
+
+   Bool $isOlympicSport; 
+
+   $motivation; 	  
+  
+   $purpose; 
+
+   $result: ['win', 'loose', 'draw'];
+
+   $skill; 
+	   
+}
+
+
+interface SportPersonLife {
+	function regime(){}; 
+
+	function healtCare();
+
+	function 
+
+
+
+
+}
+
+
+
+class SportsPerson {
+     $age;
+
+     $weight;
+
+     $height;
+
+     $health; 
+
+     $streinght; 
+
+     $sex; 
+
+     $nationality; 
+
+     $careerStart;
+
+     $careerEnd; 
+} 
+
+
+
+?>
