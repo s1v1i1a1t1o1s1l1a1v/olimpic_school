34b6542b74db0954fe852bec562c380ce10a6093
samarchenko <anton.samarchenko6@gmail.com>
2023-05-05 18:36:02 +0300

    added a shortcut icon

--- src/index.php
+++ src/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" href="assets/server.png">
     <title>Document</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Слайдер</title>
